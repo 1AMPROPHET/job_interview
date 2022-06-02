20fb34d20bc55f5f89e838211892a8fe9ee8c672
1AMPROPHET <47853509+1AMPROPHET@users.noreply.github.com>
2022-06-02 11:44:35 +0800

    diffrent deepclone

--- JS/code/deepClone.js
+++ JS/code/deepClone.js
@@ -23,12 +23,7 @@ function deepClone(obj) {
   }
 
   // 初始化返回值
-  let result
-  if (obj instanceof Array) {
-    result = []
-  } else {
-    result = {}
-  }
+  let result = Array.isArray(obj) ? [] : {}
 
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
@@ -39,3 +34,21 @@ function deepClone(obj) {
   }
   return result
 }
+
+//JSON 实现深拷贝
+function DeepClone2 (obj) {
+  let _obj = JSON.stringify(obj)
+  let objClone = JSON.parse(_obj)
+  return objClone
+  // 存在一些问题
+  // 1.无法实现对对象中方法的深拷贝，无法拷贝undefined
+  // 2.如果对象中有时间对象，则JSON.stringify后再 parse的结果，时间将只是字符串形式，而不是对象形式
+  // 3.对象中有正则、Error对象、则序列化的结果将只能得到空对象
+  // 4.对象中有NaN、Infinity，序列化后会变成null
+  // 5.JSON。stringify 只能序列化对象的可枚举的自有属性，如果对象中的对象是由构造函数生成的，会丢弃constructor
+}
+
+// jQuery 中的extend方法
+
+let arr = [1,2,3,4]
+let newArr = $.extend(true, [], arr)
